6643a7acf981ed0c25b558329bfd7b05d32b44e6
unknown <baudinopierreolivier@gmail.com>
2023-05-03 15:18:46 +0200

    add monthy with python

---
+++ index.py
@@ -0,0 +1,13 @@
+from random import random, randint
+
+print(0b101010)
+print(0b11)
+print(0o52)
+print(0x2A)
+print(bin(42))
+print(oct(42))
+print(hex(42))
+
+print(int('11111111111'))
+print(bin('0b111111111'))
+# !-----------------------------
